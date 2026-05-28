c61312571da2dcd0f16653a1a1f3fe1a66d3cdf5
ankit-mahapatra <ankitmahapatra389.gmail.com>
2026-05-28 10:58:19 +0530

    fix CORS for production

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://job-scraper-frontend.vercel.app'],
   credentials: true
 }));
 app.use(helmet());
